c44fc40438928e046cda1a01a15df86f63942c1a
jahidul96 <jahidul59895@gmail.com>
2022-11-30 18:22:18 +0600

    basic learning

---
+++ learningBasics/Test.java
@@ -0,0 +1,17 @@
+
+
+class Test {
+    public static void main(String[] args){
+       
+        int number = 23;
+        boolean isMarried = false;
+        System.out.println(number);
+        System.out.println(isMarried);
+
+        int n = 10;
+
+        for (int i = 0; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+}
