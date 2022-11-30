65c035ad3c448e134f831a8142ed3614d5083d08
jahidul96 <jahidul59895@gmail.com>
2022-11-30 18:45:23 +0600

    variable types and others!!!

---
+++ learningBasics/Variable.java
@@ -0,0 +1,18 @@
+
+class Variable {
+    public static void main(String[] args){
+        // variable defination
+
+        
+        String name = "20";
+        int age = 23;
+        float number = 20.3F;
+        boolean isAdmin = true;
+        System.out.println(name + "is" + age + "year's old!");
+        System.out.println(isAdmin);
+        System.out.println(number + age);
+
+        // instance check
+        System.out.println(name instanceof String);
+    }
+}
