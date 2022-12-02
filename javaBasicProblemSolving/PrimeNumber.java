cb4f25ac454cd9da6e6dc6349b073af8c4f6e9c1
jahidul96 <jahidul59895@gmail.com>
2022-12-02 22:03:13 +0600

    prime number check!

---
+++ javaBasicProblemSolving/PrimeNumber.java
@@ -0,0 +1,21 @@
+public class PrimeNumber {
+
+    static boolean isPrime( int n){
+        if(n<2){
+            return false;
+        }
+
+        for (int i = 2; i < n; i++) {
+            if(n%2== 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+        boolean res = isPrime(4);
+
+        System.out.println(res);
+    }
+}
