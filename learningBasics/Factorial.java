989101fa2fce03f5f961533a8c9082efa1dbe9d5
jahidul96 <jahidul59895@gmail.com>
2022-12-02 21:39:56 +0600

    recursive and foop way! factorial!

--- learningBasics/Factorial.java
+++ learningBasics/Factorial.java
@@ -1,4 +1,6 @@
 public class Factorial {
+
+    // factorial recursive way
     static int recursive(int n){
         if(n<1){
             return 1;
@@ -7,8 +9,25 @@ public class Factorial {
         }
     }
 
+
+    // factorial loop way
+    static int recFoorLoop(int x){
+        int val = 1;
+        if(x<1){
+            return 1;
+        }
+            for (int index = 1; index <= x; index++) {
+                val = val * index;
+            }
+            return val;
+    }
+
    public static void main(String[] args) {
     int res = recursive(5);
     System.out.println(res);
+
+    int value = recFoorLoop(4);
+
+    System.out.println(value);
    }
 }
