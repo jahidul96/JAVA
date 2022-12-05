968ffd46dcfb2015ef7c95cb59215d0e83ea8d09
jahidul96 <jahidul59895@gmail.com>
2022-12-05 12:10:07 +0600

    list added!

---
+++ learningBasics/App.java
@@ -0,0 +1,12 @@
+public class App {
+    enum Demo {
+        SMALL, MEDIUM
+    }
+    public static void main(String[] args) {
+
+        
+      System.out.println(Demo.MEDIUM);
+
+    }
+}
+
---
+++ learningBasics/Lists.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lists {
+    public static void main(String[] args) {
+        List<Integer> list1 = new ArrayList<>();
+
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+
+        System.out.println(list1);
+    }
+}
