a868ba9a6d74f2fdcc77b2d4babb9376b9616412
jahidul96 <jahidul59895@gmail.com>
2022-12-05 12:15:21 +0600

    added

--- learningBasics/Lists.java
+++ learningBasics/Lists.java
@@ -3,12 +3,18 @@ import java.util.List;
 
 public class Lists {
     public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
         List<Integer> list1 = new ArrayList<>();
 
+        
         list1.add(1);
         list1.add(2);
         list1.add(3);
+        list1.add(4);
 
-        System.out.println(list1);
+        list.add(10);
+        list.addAll(0, list1);
+
+        System.out.println(list);
     }
 }
